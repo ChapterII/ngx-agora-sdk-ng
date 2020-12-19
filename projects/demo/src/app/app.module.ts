22b9401adb6d82bb964e79467fc5bdd167c51a38
Saeb <s.panahifar@gmail.com>
2020-12-19 19:47:22 +0300

    Add routing

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -4,19 +4,14 @@ import { NgxAgoraSdkNgModule } from 'ngx-agora-sdk-ng';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
-import { VideoComponent } from './video/video.component';
+import { VideoComponent } from './pages/video/video.component';
 import { MeetingPreviewComponent } from './pages/meeting-preview/meeting-preview.component';
 import { RoundTranparentIconButtonComponent } from './shared/components/round-tranparent-icon-button/round-tranparent-icon-button.component';
 import { InputOutputSettingsComponent } from './shared/components/input-output-settings/input-output-settings.component';
 import { SoundMeterComponent } from './shared/components/sound-meter/sound-meter.component';
 import { CameraPreviewComponent } from './shared/components/camera-preview/camera-preview.component';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  { path: '', redirectTo: 'preview', pathMatch: 'full' },
-  { path: 'preview', component: MeetingPreviewComponent },
-  { path: 'sample', component: VideoComponent },
-]
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -27,7 +22,8 @@ const routes: Routes = [
     RoundTranparentIconButtonComponent,
     InputOutputSettingsComponent,
     SoundMeterComponent,
-    CameraPreviewComponent
+    CameraPreviewComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +32,7 @@ const routes: Routes = [
       Video: { codec: 'h264', mode: 'rtc', role: 'host' }
     }),
     FontAwesomeModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
