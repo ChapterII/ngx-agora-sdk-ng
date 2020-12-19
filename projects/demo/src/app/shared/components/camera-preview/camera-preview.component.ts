34e67e59a21f62db5e62b8098bbbb4cd6e2fde91
Saeb <s.panahifar@gmail.com>
2020-12-19 16:14:40 +0300

    add jquery to package

--- projects/demo/src/app/shared/components/camera-preview/camera-preview.component.ts
+++ projects/demo/src/app/shared/components/camera-preview/camera-preview.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-camera-preview',
   templateUrl: './camera-preview.component.html',
   styleUrls: ['./camera-preview.component.css']
 })
-export class CameraPreviewComponent implements OnInit {
+export class CameraPreviewComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('camera') camera!: ElementRef;
 
   constructor() { }
 
+  ngAfterViewInit(): void {
+   
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+      this.camera.nativeElement.srcObject = stream;
+    });
+    
+  }
+
   ngOnInit(): void {
+
+
+
   }
 
 }
